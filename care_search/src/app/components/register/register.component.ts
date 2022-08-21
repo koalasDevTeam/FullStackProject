9eed15951b3f2bf1354b0aed02a0a334d5ed175a
Zamara Reyes <zamarareyes5@gmail.com>
2022-08-21 18:20:16 +0100

    fourth commit

--- care_search/src/app/components/register/register.component.ts
+++ care_search/src/app/components/register/register.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-register',
@@ -11,18 +12,29 @@ export class RegisterComponent implements OnInit {
   public emailUser: string = "";
   public passwordUser: string = "";
   public privacyUser: string = "";
+  public emptyForm: string = "";
+  public emptyPrivacy: string = "";
 
-  constructor() { }
+  constructor(private router: Router) { }
 
   ngOnInit(): void {
   }
 
   save() {
-    this.user.push({
-      name: this.emailUser,
-      password: this.passwordUser,
-      privacy: this.privacyUser
-    });
+    if((this.emailUser == "") || (this.passwordUser == "")){
+      this.emptyForm = "Error en el registro, debe rellenar los campos vacíos.";
+      this.emptyPrivacy = "";
+    } else if(this.privacyUser == "") {
+      this.emptyForm = "";
+      this.emptyPrivacy = "Debe aceptar la Política de Privacidad.";
+    } else {
+      this.user.push({
+        name: this.emailUser,
+        password: this.passwordUser,
+        privacy: this.privacyUser
+      });
+      this.router.navigate(['/profile']);
+    }
     console.log(this.user);
   }
 
