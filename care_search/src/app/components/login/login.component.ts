316d7cbdf538cf65b1e18440641953dd0fa05b55
Zamara Reyes <zamarareyes5@gmail.com>
2022-08-24 21:44:31 +0100

    arreglar login y registro

--- care_search/src/app/components/login/login.component.ts
+++ care_search/src/app/components/login/login.component.ts
@@ -10,7 +10,7 @@ export class LoginComponent implements OnInit {
   public user: any = [];
   public emailUser: string = "";
   public passwordUser: string = "";
-  
+  public emptyForm: string = "";
   
   constructor(private router: Router) { }
 
@@ -18,7 +18,9 @@ export class LoginComponent implements OnInit {
   }
 
   save() {
-    
+    if((this.emailUser == "") || (this.passwordUser == "")){
+      this.emptyForm = "Error en las credenciales. Usuario no registrado.";
+    } else {
       this.user.push({
         name: this.emailUser,
         password: this.passwordUser,
@@ -29,6 +31,8 @@ export class LoginComponent implements OnInit {
 
       //console.log(this.user);
     }
+
+  }
     
   
 
