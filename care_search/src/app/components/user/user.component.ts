0f664d026307a5a5741b08e6a7c8aee5774862e1
abigailojeda <abigail.arte@gmail.com>
2022-08-27 21:20:07 +0100

    user photo preview

--- care_search/src/app/components/user/user.component.ts
+++ care_search/src/app/components/user/user.component.ts
@@ -1,4 +1,6 @@
+import { ReadVarExpr } from '@angular/compiler';
 import { Component, OnInit } from '@angular/core';
+import { DomSanitizer } from '@angular/platform-browser';
 import { UsersService } from '../../services/users/users.service';
 
 @Component({
@@ -8,12 +10,24 @@ import { UsersService } from '../../services/users/users.service';
 })
 export class UserComponent implements OnInit {
 
-  constructor(private UsersService:UsersService) { }
+
+
   public storage: any;
   public pageSelected : string = 'intro'
   public userIntroIllustration: string = "./assets/img/illustrations/user_intro_illustration.svg"
   public noUserIllustration: string = "./assets/img/illustrations/no_user_illustration.svg"
   public photoUser: string = "";
+  public previewPhoto: string='./assets/img/illustrations/no_found_illustration_mb.svg';
+
+  constructor(
+    private UsersService:UsersService,
+    private sanitizer:DomSanitizer
+    ) { }
+
+
+  //aqui se guarda el archivo de la imagen:
+  public photoFile: any = [];
+ 
 
   ngOnInit(): void {
     this.storage = this.UsersService.storage;
@@ -30,8 +44,38 @@ export class UserComponent implements OnInit {
     this.pageSelected = 'notifications'
   }
 
+//to show preview photo:
+  takeNewPicture(event:any): any{
+    const file = event.target.files[0]
+    this.toBase64(file).then((img: any) =>{
+      this.previewPhoto = img.base;
+      //console.log(img)
+    })
+    this.photoFile.push(file)
 
+  }
 
+  toBase64 = async ($event: any) => new Promise((resolve, reject) => {
+    try {
+      const unsafeImg = window.URL.createObjectURL($event);
+      //const image = this.sanitizer.bypassSecurityTrustUrl(unsafeImg);
+      const reader = new FileReader();
+      reader.readAsDataURL($event);
+      reader.onload = () => {
+        resolve({
+          base: reader.result
+        });
+      };
+      reader.onerror = error => {
+        resolve({
+          base: null
+        });
+      };
 
+      return;
 
+    } catch (e) {
+      return null;
+    }
+  })
 }
