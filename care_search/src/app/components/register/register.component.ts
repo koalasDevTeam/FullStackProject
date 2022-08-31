44684d4af60366ebd69b085c483c9a9cda97eaf5
Sabrina-MM <sabrinamarisolmartinez@gmail.com>
2022-08-31 21:50:18 +0100

    checking if it is a user or a professional, and if it is already registed

--- care_search/src/app/components/register/register.component.ts
+++ care_search/src/app/components/register/register.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 import { UsersService } from '../../services/users/users.service';
 
+
 @Component({
   selector: 'app-register',
   templateUrl: './register.component.html',
@@ -15,14 +16,42 @@ export class RegisterComponent implements OnInit {
   public emptyForm: string = '';
   public emptyPrivacy: string = '';
   public newUser: any = {};
+  public iAmAProfessional: boolean =true
+  public users:any
+ 
 
-  constructor(private router: Router, private userService: UsersService) {}
+  constructor(
+    private router: Router, 
+    private userService: UsersService, 
+   
+    ) {
 
-  ngOnInit(): void {}
+    }
 
+  ngOnInit(): void {this.getAllUsers()}
+
+  checkingIfProfessional(){
+  this.iAmAProfessional=false
+  console.log(`i'm a professional = ${this.iAmAProfessional}`)
+  }
+  chekingIfUser(){
+    this.iAmAProfessional=true
+    console.log(`i'm a professional = ${this.iAmAProfessional}`)
+  }
+ getAllUsers(){
+    return this.userService.getAllUsers().then((response:any) => console.log(this.users = response)); 
+    
+  }
   validateAndCreateUserWithCredentials() {
+    //check if exist already an equal email and password in the json 
+    const result = this.users.filter((user:any)=> user.email === this.emailUser && user.pass === this.passwordUser)
+    if(result.length > 0){
+      console.log(`you're aready registed`)
+      return 
+    }
     this.emptyForm = '';
     this.emptyPrivacy = '';
+    console.log(`i'm profileUser: ${this.profileUser}`)
     if ((this.emailUser == '') || (this.passwordUser == '') || (this.profileUser == '')) {
       this.emptyForm = 'Error en el registro, debe rellenar los campos vacíos.';
     } else if (this.privacyUser == '') {
@@ -33,7 +62,7 @@ export class RegisterComponent implements OnInit {
         pass: `${this.passwordUser}`,
         name: '',
         work: '',
-        worker:true,
+        worker:`${this.iAmAProfessional}`,
         img: './assets/img/users/1.jpg',
         full_info:'',
         score: 0,
@@ -41,11 +70,13 @@ export class RegisterComponent implements OnInit {
         profile: ''
       };
 
+
       this.userService.createNewUser(this.newUser).then((user) => {
         localStorage.setItem('currentUser', JSON.stringify(user))
         this.userService.setCurrentUser(user)
         this.router.navigate(['/user']);
       });
     }
+    
   }
 }
